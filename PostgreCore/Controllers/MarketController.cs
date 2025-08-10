3be99ff5c3be07358b54dfd02a4d6500c4b8280a
WarLock54 <onr_540@hotmail.com>
2025-08-10 17:40:18 +0300

    adding contoller

---
+++ PostgreCore/Controllers/MarketController.cs
@@ -0,0 +1,8 @@
+﻿using Business;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace PostgreCore.Controllers
+{
+    public partial class MarketController : DaBDataController<Market, long, MarketBs> { };
+}
